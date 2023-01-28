bc276a639be14d6ff5442c7586caf03dee323599
vklynn <vickylynnmauldin@Vickylynns-MBP.lan>
2023-01-28 16:38:39 -0500

    started weather portion of code will need to complete pt2

--- Starter_Code/starter_code/api_keys.py
+++ Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "4725939bb13abacc9dd7d495b0784a1d"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "f567f138211d49b4b05cc3c2ed3aba57"
